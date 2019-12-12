f4f17b2b1a32a09e5661e34be45ef9cbcf53a37d
COE-029 <coe-029@coe-029s-imac.local>
2019-12-12 13:23:07 +0530

    Initial Commit

---
+++ UI_cordata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UI_cordata
+//
+//  Created by COE-029 on 12/12/19.
+//  Copyright © 2019 COE-029. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
